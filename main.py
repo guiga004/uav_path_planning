0226f3782fb4f1b04952ce10ac2acbda160c0ef7
guiga004 <guiga004@umn.edu>
2020-01-29 19:05:57 -0600

    fixed splitting algorithm

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ email  : guiga004@umn.edu
 
 import numpy as np
 import time
+import copy
 from environment import Environment
 from draw import Draw
 from tsp_algorithms import ant_tsp, genetic_tsp, exact_tsp
@@ -48,10 +49,26 @@ def get_uav_routes(environment, number_of_uavs):
     # append 2*pi to include the points belonging to the last drone
     angles.append(np.radians(360))
 
+    optimal_split = round(len(environment.cities) / (len(angles)-1))
+
+    print(f'angles: {angles}')
+    print(f'cities: {environment.cities}')
+    print(f'optimal_split: {optimal_split}')
+
+    # sort the cities by their angle
+    for city in environment.cities:
+        city.append(environment.find_angle_from_center(city))
+
+    print(f'cities: {environment.cities}')
+
+    environment.cities = sorted(environment.cities, key=lambda e: e[2], reverse=False)
+
+    print(f'cities: {environment.cities}')
+
     # calculate the angle of each point and assign them to a respective drone
     for city in environment.cities:
 
-        city_angle = environment.find_angle_from_center(city)
+        city_angle = city[2]
 
         # don't care about the center since we are starting at the center
         if city_angle != 'center':
@@ -63,12 +80,23 @@ def get_uav_routes(environment, number_of_uavs):
 
                     # create a key in the dictionary if not yet created+
                     if f'{i}' not in uav_routes.keys():
-                        uav_routes.update({f'{i}': [city]})
+                        uav_routes.update({f'{i}': [city[:2]]})
 
                     else:
-                        uav_routes[f'{i}'].append(city)
+
+                        if len(uav_routes[f'{i}']) < optimal_split or angle == angles[-1]:
+                            uav_routes[f'{i}'].append(city[:2])
+
+                        else:
+
+                            if f'{i+1}' not in uav_routes.keys():
+                                uav_routes.update({f'{i+1}': [city[:2]]})
+                            else:
+                                uav_routes[f'{i+1}'].append(city[:2])
+
 
                     break
+
 
     return uav_routes, rotated_points
 
@@ -77,7 +105,7 @@ if __name__ == "__main__":
 
     m = 2  # width
     n = 3  # height
-    k = 2  # number of uavs
+    k = 3  # number of uavs
 
     land = Environment(width=m, height=n)
     cities = land.get_cities
@@ -89,6 +117,7 @@ if __name__ == "__main__":
     # only run the following code if number of UAVs is greater than 0
     if k > 0:
 
+        total_path_length = 0
         start_time = time.time()
 
         # this will split up the uav routes
@@ -109,9 +138,10 @@ if __name__ == "__main__":
             # have each uav travel back to the center point
             path.append(path[0])
 
-            picasso.draw_path(path=path, color=colors[i])
+            total_path_length += picasso.draw_path(path=path, color=colors[i])
 
         print("--- %s seconds ---" % (time.time() - start_time))
+        print("Total Length", total_path_length)
 
         # this will visualize how the environment was split
         picasso.draw_split(split)
