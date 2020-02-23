66ef5b6e8a8e161175bf0d4a36c343289b7befbf
guiga004 <guiga004@umn.edu>
2020-02-22 22:54:46 -0600

    added color functionality to the environment partition

--- testing_grounds.py
+++ testing_grounds.py
@@ -1,7 +1,28 @@
+import random
 import math
 from draw import Draw
 from tsp_algorithms import exact_tsp
 import matplotlib.pyplot as plt
+
+
+def get_random_color(pastel_factor=0.5):
+    return [(x + pastel_factor) / (1.0 + pastel_factor) for x in [random.uniform(0, 1.0) for i in [1, 2, 3]]]
+
+def color_distance(c1, c2):
+    return sum([abs(x[0] - x[1]) for x in zip(c1, c2)])
+
+def generate_new_color(existing_colors, pastel_factor=0.5):
+    max_distance = None
+    best_color = None
+    for i in range(0, 100):
+        color = get_random_color(pastel_factor=pastel_factor)
+        if not existing_colors:
+            return color
+        best_distance = min([color_distance(color, c) for c in existing_colors])
+        if not max_distance or best_distance > max_distance:
+            max_distance = best_distance
+            best_color = color
+    return best_color
 
 def partitioning(a1, a2, x, y):
     '''
@@ -18,21 +39,20 @@ def partitioning(a1, a2, x, y):
     for k1 in range(1, math.ceil(x/a1)):
         for k2 in range(1, math.ceil(y/a2)):
             i = i+1
-            P.append([[(k1-1)*a1, k1*a1], [(k2-1)*a2, k2*a2]])
+            P.append([[(k1-1)*a1, k1*a1], [(k2-1)*a2, k2*a2], 9])
 
     for k in range(1, math.ceil(y/a2)):
         i = i+1
-        P.append([[x-a1, x], [(k-1)*a2, k*a2]])
+        P.append([[x-a1, x], [(k-1)*a2, k*a2], 6])
 
     for k in range(1, math.ceil(x/a1)):
         i = i+1
-        P.append([[(k-1)*a1, k*a1], [y-a2, y]])
+        P.append([[(k-1)*a1, k*a1], [y-a2, y], 3])
 
     i = i+1
-    P.append([[x-a1, x], [y-a2, y]])
+    P.append([[x-a1, x], [y-a2, y], 0])
 
     return P
-
 
 
 def draw_everything(a1, a2, x, y, hardware_specs=None):
@@ -42,13 +62,33 @@ def draw_everything(a1, a2, x, y, hardware_specs=None):
 
     partitions = partitioning(a1, a2, x, y)
 
+    colors = []
+    for i in range(len(partitions)):
+        colors.append(generate_new_color(colors, pastel_factor=0.9))
+
     for partition in partitions:
+
         bottom_corner = (partition[0][0], partition[1][0])
         width_x = partition[0][1] - partition[0][0]
         height_y = partition[1][1] - partition[1][0]
         partition_midpoints.append((bottom_corner[0] + width_x/2, bottom_corner[1] + height_y/2))
 
-        rectangle = plt.Rectangle(xy=bottom_corner, width=width_x, height=height_y, fill=False, color='white', ec='black', lw=4)
+
+        edge_color = 'black'
+        order = partition[2]
+        line_width = 3
+
+        rectangle = plt.Rectangle\
+            (
+                xy=bottom_corner,
+                width=width_x,
+                height=height_y,
+                fill=True,
+                color=colors.pop(),
+                ec=edge_color,
+                lw=line_width,
+                zorder=order
+            )
         picasso.draw.add_patch(rectangle)
 
     return picasso, partition_midpoints
@@ -58,27 +98,37 @@ if __name__ == "__main__":
 
     big_x = 11
     big_y = 9
-    small_x = 3
-    small_y = 3
-
-    test, ugv_points = draw_everything(big_x, big_y, small_x, small_y)
-
-    # run tsp for the ugv route
-    ugv_path = exact_tsp(ugv_points)
-
-    # have the ugv
-
-    ugv_path.append(ugv_path[0])
-
-    total_path_length = test.draw_path(path=ugv_path, color='red')
-
-
-    test.show_fig()
+    # small_x = 3
+    # small_y = 3
 
     # print out all possible combinations of partition sizes
 
-    for y in range(1, big_y):
+    partition_sizes = []
 
-        for x in range(1, big_x):
+    for y in range(3, 4):
 
-            print(f'partition size: {x} x {y}')
+        for x in range(3, 4):
+            partition_sizes.append([x, y])
+
+    for partition_size in partition_sizes:
+
+        # print(partition_size)
+
+        small_x = partition_size[0]
+        small_y = partition_size[0]
+
+        test, ugv_points = draw_everything(small_x, small_y, big_x, big_y)
+
+        # run tsp for the ugv route
+        ugv_path = exact_tsp(ugv_points)
+
+        # have the ugv
+
+        ugv_path.append(ugv_path[0])
+
+        total_path_length = test.draw_path(path=ugv_path, color='white')
+
+        test.show_fig()
+
+
+
