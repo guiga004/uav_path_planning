19f2025c3e8d3c83ec59c907be7ed54a89eb9a5b
Mohammed Guiga <guiga004@umn.edu>
2020-05-08 20:55:07 -0500

    fixed circular imports

--- tools/paper_algorithms.py
+++ tools/paper_algorithms.py
@@ -3,12 +3,34 @@ author : Mohammed Guiga
 email  : guiga004@umn.edu
 """
 import math
+import random
 from tools.environment import Environment
-import testing_grounds as tg
 import tools.guiga_algorithms as gumo
 import matplotlib.pyplot as plt
 from tsp_algorithms.tsp_algorithms import exact_tsp
 from tools.draw import Draw
+
+
+def get_random_color(pastel_factor=0.5):
+    return [(x + pastel_factor) / (1.0 + pastel_factor) for x in [random.uniform(0, 1.0) for i in [1, 2, 3]]]
+
+
+def color_distance(c1, c2):
+    return sum([abs(x[0] - x[1]) for x in zip(c1, c2)])
+
+
+def generate_new_color(existing_colors, pastel_factor=0.5):
+    max_distance = None
+    best_color = None
+    for i in range(0, 100):
+        color = get_random_color(pastel_factor=pastel_factor)
+        if not existing_colors:
+            return color
+        best_distance = min([color_distance(color, c) for c in existing_colors])
+        if not max_distance or best_distance > max_distance:
+            max_distance = best_distance
+            best_color = color
+    return best_color
 
 '''
 Taken from:
@@ -187,7 +209,7 @@ def uav_ugv_trajectory_generation(x_bar, y_bar, specs=None, obstacles=[]):
 
     colors = []
     for _ in range(len(min_partitions)):
-        colors.append(tg.generate_new_color(colors, pastel_factor=0.9))
+        colors.append(generate_new_color(colors, pastel_factor=0.9))
 
     first_color = colors[0]
 
